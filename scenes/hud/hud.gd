dd882a89b4b68d222706a5396c1ef902d1ec49b8
Artur Nizamov <mreclipse27@gmail.com>
2024-06-10 22:08:07 +0500

    Dodge the Creeps! game

---
+++ scenes/hud/hud.gd
@@ -0,0 +1,37 @@
+extends CanvasLayer
+
+signal start_game
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game Over")
+	# Wait until the MessageTimer has counted down.
+	await $MessageTimer.timeout
+
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+	# Make a one-shot timer and wait for it to finish.
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+
+func _on_mesage_timer_timeout():
+	$Message.hide()
